d8ba0d8b00f9968933c8f86a544d8ac03daab61e
Ava Melchior <avacmelchior@gmail.com>
2023-08-30 16:13:46 -0500

    javascript prompts

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log('app js is running!');
+
+let userName = prompt('What is your name?');
+console.log('username', userName)
+
+let message
+message = 'Welcome to the Wild West,'
+document.write(message + ' ' + userName + '!')
+
+confirm('Pretty cool, right?')
+
